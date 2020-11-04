c310a145c9a98cddb0c9d1d3fd228d5d101ffe2f
Trithuan <36200155+Trithuan@users.noreply.github.com>
2020-11-04 22:00:23 +0100

    nom des cases

--- Chess_3D/Assets/Board.cs
+++ Chess_3D/Assets/Board.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
 	GameObject[,] TileArray  = new GameObject[8,8];
 	// Start is called before the first frame update
+	float taille_cases = 100;
+	float épaisseur_cases = 1;
+
 	void Start()
 	{
 		for(int i = 0; i < 8; i++)
@@ -13,9 +18,10 @@ public class Board : MonoBehaviour
 			for(int j = 0; j < 8; j++)
 			{
 				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-				cube.transform.position = new Vector3(i+(0.1f*i), 0, j+(0.1f*j));
-				cube.transform.localScale  = new Vector3(1, 0.1f, 1);
+				cube.transform.position = new Vector3(i*taille_cases, 0, j*taille_cases);
+				cube.transform.localScale  = new Vector3(taille_cases, épaisseur_cases, taille_cases);
 				cube.transform.parent = gameObject.transform;
+				cube.name = char.ConvertFromUtf32(65+i) +  (j+1);
 				if((i + j) % 2 == 1){
 				cube.GetComponent<Renderer>().material.color = Color.black;
 				}
