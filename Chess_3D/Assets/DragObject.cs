60cae7549f318a1aa6db60ebcd4689754b52fdf2
Trithuan <36200155+Trithuan@users.noreply.github.com>
2020-11-05 17:51:11 +0100

    jump size 200

--- Chess_3D/Assets/DragObject.cs
+++ Chess_3D/Assets/DragObject.cs
@@ -6,7 +6,7 @@ public class DragObject : MonoBehaviour
 {
 	private Vector3 mOffset;
 	private float mZCoord;
-	private int jump_size = 150;
+	private int jump_size = 200;
 	private float static_height = 0;
 
 
